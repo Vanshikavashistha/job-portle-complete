ffb16245454f2c5607bd92457ddb80355940a85c
Vanshika Vashistha <vanshikavashistha@Vanshikas-MacBook-Air.local>
2026-07-21 07:03:06 +0530

    fix cors origin

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: '*',
+    origin: 'https://job-portle-complete.vercel.app',
     credentials:true
 }
 
